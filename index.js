c6dd2027c5d4499d0c204c7044ab2b4761c6787c
Phan Anh Tuan <phananhtuan1011@gmail.com>
2019-03-13 18:13:06 +0700

    Count of Source and display it on the navigation section of the site

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 let allNews = [];
 let pageNumber = 1;
 const moreButton = document.getElementById("more");
-const currentNumberOfStories = document.getElementById('currentNumberOfStories');
+const currentNumberOfStories = document.getElementById(
+  "currentNumberOfStories"
+);
+const newsCategory = document.getElementById("newsCategory");
 
 const fetchNews = async () => {
   const API_KEY = "8be8520c430d410785329e09e4aab662";
-  const url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}&pageSize=1&page=${pageNumber}`;
-  console.log(url);
+  const url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}&pageSize=10&page=${pageNumber}`;
   const json = await fetch(url).then(response => response.json());
-  allNews = allNews.concat(json.articles)
-  console.log(allNews);
+  allNews = allNews.concat(json.articles);
   render(allNews);
 };
 
 const render = newsArray => {
   document.getElementById("display").innerHTML = renderNews(newsArray);
   currentNumberOfStories.innerHTML = allNews.length;
+  newsCategory.innerHTML = renderCategory(newsArray);
 };
-
-
-//Click for more button
-moreButton.addEventListener("click", () => {
-  pageNumber++;
-  console.log(`Page Number: ${pageNumber}`);
-  fetchNews();
-});
 
 const renderNews = newsArray => {
   if (newsArray) {
-    return newsArray.map(article => {
-      return `<div class="col-4">
+    return newsArray
+      .map(article => {
+        return `<div class="col-6">
         <div class="card mb-2">
             <img src="${article.urlToImage}" 
                  alt="${article.urlToImage}"
@@ -43,10 +38,42 @@ const renderNews = newsArray => {
             </div>
         </div>
     </div>`;
-    }).join('');
+      })
+      .join("");
   }
 };
 
+const renderCategory = newsArray => {
+  let result = {};
+  console.log(newsArray);
 
-render(allNews);
+  //from NewsArray to Category Object (with source: count keys/value) 
+  for (let i = 0; i < newsArray.length; i++) {
+    if (result[newsArray[i].source.name]) {
+      result[newsArray[i].source.name] = result[newsArray[i].source.name] + 1;
+    } else {
+     result[newsArray[i].source.name] = 1;
+    }
+  }
+
+  //render that Category Object
+  return Object.keys(result).map(source => { 
+      return `
+        <div class='checkbox'> 
+            <label> 
+                <input type='checkbox' value="${source}" /> 
+                ${source} (${result[source]})
+            </label>
+        </div>
+      `}).join('');
+};
+
+//Click for more button
+moreButton.addEventListener("click", () => {
+  pageNumber++;
+  console.log(`Page Number: ${pageNumber}`);
+  fetchNews();
+});
+
+// render(allNews);
 fetchNews();
