eab169f87ef3ad8991f81ab594e14d4d393c9fe0
Phan Anh Tuan <phananhtuan1011@gmail.com>
2019-03-14 11:43:00 +0700

    Fixing the UI, Enable Masonry Columns

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let allNews = [];
 let pageNumber = 1;
-let pageSize = 5;
+let pageSize = 10;
 let category = "technology";
 const moreButton = document.getElementById("more");
 const currentNumberOfStories = document.getElementById(
@@ -10,6 +10,7 @@ const newsSource = document.getElementById("newsSource");
 const categoryCheckbox = document.getElementsByName("categoryCheckbox");
 const categoryMenu = document.getElementById("categoryMenu");
 const dropdownItem = document.getElementsByClassName("dropdown-item");
+let sourceSelected = [];
 const categoryList = [
   "business",
   "entertainment",
@@ -19,16 +20,15 @@ const categoryList = [
   "sports",
   "technology"
 ];
-let sourceSelected = [];
 
-const fetchNews = async (from) => {
+const fetchNews = async from => {
   const API_KEY = "8be8520c430d410785329e09e4aab662";
   const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&pageSize=${pageSize}&page=${pageNumber}&category=${category}`;
 
   const json = await fetch(url).then(response => response.json());
-  if (from === 'dropdown') { 
-      allNews = json.articles;
-  } else { 
+  if (from === "dropdown") {
+    allNews = json.articles;
+  } else {
     allNews = allNews.concat(json.articles);
   }
   render(allNews);
@@ -36,9 +36,9 @@ const fetchNews = async (from) => {
 
 const render = (newsArray, from) => {
   //renderNews & renderSource should be independent?
-  console.log(sourceSelected);
+  console.log(newsArray);
   let filteredNewsArray;
-  if (sourceSelected.length == 0 ) {
+  if (sourceSelected.length == 0) {
     filteredNewsArray = newsArray;
   } else {
     filteredNewsArray = newsArray.filter(article =>
@@ -47,11 +47,12 @@ const render = (newsArray, from) => {
   }
   document.getElementById("display").innerHTML = renderNews(filteredNewsArray);
   currentNumberOfStories.innerHTML = newsArray.length;
-  
+
   categoryMenu.innerHTML = renderCategory();
   handleDropdown();
-  
-  if (!from) { //This is to skip the Checkbox if we call the render function inside the checkbox click
+
+  if (!from) {
+    //This is to skip the Checkbox if we call the render function inside the checkbox click
     newsSource.innerHTML = renderSource(newsArray);
     handleCheckbox();
   } else {
@@ -63,20 +64,18 @@ const renderNews = newsArray => {
   if (newsArray) {
     return newsArray
       .map(article => {
-        return `<div class="col-xs col-md-6">
-        <div class="card mb-2">
+        return `<div class="card mb-4 rounded-5">
             <img src="${article.urlToImage}" 
                  alt="${article.urlToImage}"
                  width= '100%'
                  height= auto
                  max-height = '300px'/>
+            <div class="card-header"><a class='text-decoration-none' href=${article.url}><h3>${article.title}</h3></a></div>
             <div class="card-body">
-                <div class="card-title"><h3>${article.title}</h3></div>
-                <p>${moment(article.publishedAt).fromNow()}</p>
+                <p>${moment(article.publishedAt).fromNow()} - <b>${article.source.name}</b> - <b>${article.author ? article.author : ''}</b> </p>
                 <div class="card-text">${article.description}</div>
             </div>
-        </div>
-    </div>`;
+        </div>`;
       })
       .join("");
   }
@@ -87,7 +86,7 @@ const renderSource = newsArray => {
   //from NewsArray to Category Object (with source: count keys/value)
   for (let i = 0; i < newsArray.length; i++) {
     if (result[newsArray[i].source.name]) {
-      result[newsArray[i].source.name] = result[newsArray[i].source.name] + 1;
+      result[newsArray[i].source.name]++;
     } else {
       result[newsArray[i].source.name] = 1;
     }
@@ -129,7 +128,6 @@ const handleCheckbox = () => {
           source => source !== eachBox.value
         );
       }
-      console.log(sourceSelected);
       render(allNews, "checkbox");
     })
   );
@@ -139,9 +137,16 @@ const handleDropdown = () => {
   [...dropdownItem].forEach(item =>
     item.addEventListener("click", () => {
       category = item.innerHTML;
-      fetchNews('dropdown');
+      changeUrl(category);
+    //   fetchNews('dropdown');
     })
   );
+};
+
+const changeUrl = category => {
+  let url = new URL(window.location.href);
+  url.searchParams.append("category", category);
+  window.location.href = url.toString();
 };
 
 //Click for more button
